99c65bf06d304dea540e517f14a1d4134d68e6c1
bhoeft <brandonmhoeft@gmail.com>
2022-10-09 14:22:32 -0500

    add: initial insert script examples from game 1.

--- sql/insert_scripts.sql
+++ sql/insert_scripts.sql
@@ -66,3 +66,85 @@ insert into games (fk_season_id, game_num, game_minutes, home_or_away, fk_opp_id
 values (1, 1, 8,'home', '13', 1, 3.8, 54, 16, 13, 3, 1.8, 9, 4);
 
 
+insert into player_stats_overall (
+    fk_player_id,
+    fk_game_id,
+    rating,
+    minutes,
+    total_dist,
+    sprint_dist)
+values
+    (2, 1, 8.1, 93, 1, 0), -- Goalie: Mamardashvili
+    (20, 1, 7, 93, 10.3, 2), -- Lino
+    (22, 1, 7.3, 27, 2.6, 0.7), -- Duro
+    (24, 1, 7.4, 66, 6.8, 1.3), -- Cavani
+    (11, 1, 5.9, 12, 1.2, 0.5), -- Lato
+    (21, 1, 7.4, 81, 9.6, 2.3), -- Castillejo
+    (16, 1, 6, 16, 1.8, 0.6), -- Nico Gonzalez
+    (15, 1, 7, 77, 8.9, 1.7), -- Musah
+    (18, 1, 6.3, 93, 11.1, 3.1), --Almeida
+    (14, 1, 6.7, 93, 9.9, 1.6), -- Guillamon
+    (9, 1, 6.9, 93, 10.4, 3.1), --gaya
+    (4, 1, 6.9, 93, 10, 1.7), --Diakhaby
+    (7, 1, 5.7, 27, 2.7, 0.5), -- Comert
+    (5, 1, 6.3, 66, 6.4, 0.5), -- Paulista
+    (12, 1, 6.6, 93, 10.7, 3) -- Correia
+
+
+insert into player_stats_off (
+    fk_player_id,
+    fk_game_id,
+    possession_pct,
+    dribble_att,
+    dribble_compl,
+    dribble_dist,
+    dribble_beat,
+    dribble_knock_on,
+    goals,
+    non_pen_xg,
+    shots,
+    shots_on_targ,
+    assists,
+    xa,
+    second_assists,
+    key_passes,
+    players_beat_by_passes,
+    passes_att,
+    passes_compl,
+    lobs_compl,
+    through_balls_compl,
+    crosses_att,
+    crosses_compl)
+values
+    (20, 1, 3, 12, 12, 0.3, 1, 4, 0, 0, 1, 0,0,0.1,2, 2, 6, 12,11, 0, 3, 1, 0), -- Lino
+    (22, 1, 2, 4, 4, 0.1, 0, 2, 1, 0.8, 2,2, 0,0,0,0,1, 3,3,0,0,0,0), -- Duro
+    (24, 1, 6, 14, 13, 0.3, 2, 5, 2, 1.0, 3, 2, 0,0,0,0, 4, 15, 14,0, 3, 0, 0), -- Cavani
+    (11, 1, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), -- Lato
+    (21, 1, 6, 15, 11, 0.5, 1, 8, 0, 0, 0, 0, 0, 0.5, 0, 0, 2, 13, 9, 0, 2, 3, 0), -- Castillejo
+    (16, 1, 0, 1, 1,0, 0,0,0,0,0,0,1,0.1,0,1, 5, 2, 1,0, 1,0, 0), -- Nico Gonzalez
+    (15, 1, 5, 20, 19, 0.4, 2, 2, 0,0,0,0,1, 0.5, 0, 2, 15, 23, 19, 1, 4, 0,0), -- Musah
+    (18, 1, 3, 14, 13, 0.3, 0, 2, 0, 0, 0,0,0,0,0,0,0,16, 12, 0,1,1,0), --Almeida
+    (14, 1, 3, 14,11,0.2,0,1,0,0,1,0,0,0.1,0,1,8, 12,12,0,2,0,0), -- Guillamon
+    (9, 1, 6,10,10,0.6, 2, 7,0,0,2,0,0,0,0,0,3,12,7,0,0,1,0), --gaya
+    (4, 1, 2,7,7,0.1,3,0,0,0,0,0,0,0,0,0,2,9,8,0, 2,0,0), --Diakhaby
+    (7, 1, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), -- Comert
+    (5, 1, 1, 2,2,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,0,0), -- Paulista
+    (12, 1, 5,11,10,0.5,0,7,0,0,0,0,0,0,0,0,4,12,10,0,2,0,0) -- Correia
+
+
+insert into player_stats_def (fk_player_id, fk_game_id, stand_tkl_att, stand_tkl_won, slide_tkl_att, slide_tkl_won, interceptions, blocks, clearances, off_duels_att, off_duels_won, def_duels_att, def_duels_won, air_duels_att, air_duels_won, beaten_by_opp, fouls_committed)
+values
+    (20, 1,0,0,2,0,0,0,0,1,0,0,0,5,1,0,0), -- Lino
+    (22, 1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0), -- Duro
+    (24, 1,2,0,2,0,0,0,0,4,2,2,0,2,0,1,1), -- Cavani
+    (11, 1, 0,0,0,0,0,0,0,0,0,1,0,1,0,0,0), -- Lato
+    (21, 1, 4,1,1,0,1,0,0,11,4,3,3,0,0,1,0), -- Castillejo
+    (16, 1, 0,0,0,0,1,0,0,1,0,0,0,0,0,0,0), -- Nico Gonzalez
+    (15, 1, 3,0,0,0,3,0,0,4,1,5,2,1,1,2,0), -- Musah
+    (18, 1, 7,1,0,0,0,0,1,2,0,13,4,3,1,4,0), --Almeida
+    (14, 1, 6,0,1,0,6,0,0,1,0,12,4,0,0,3,0), -- Guillamon
+    (9, 1, 1,1,1,0,0,0,0,2,1,5,3,0,0,2,0), --gaya
+    (4, 1, 2,0,0,0,3,0,0,2,2,4,2,2,0,0,0), --Diakhaby
+    (7, 1, 0,0,1,0,0,0,0,0,0,2,1,0,0,1,0), -- Comert
+    (5, 1, 2,1,0,0,0,0,0,0,0,6,3,0,0,2,0), -- Paulista
+    (12, 1,1,0,0,0,0,0,0,1,1,3,1,0,0,3,0) -- Correia

824357795257dd720cae56fb607f0a385d8762d5
bhoeft <brandonmhoeft@gmail.com>
2022-10-09 01:56:47 -0500

    add: SQL scripts of insert examples

---
+++ sql/insert_scripts.sql
@@ -0,0 +1,68 @@
+--  query templates for loading tables
+insert into leagues (name)
+values
+    ('La Liga'),
+    ('Copa Del Rey');
+
+insert into teams (name)
+values ('Valencia CF'),
+       ('Real Madrid'),
+       ('Barcelona'),
+       ('Athletic Club'),
+       ('Atletico Madrid'),
+       ('Real Betis'),
+       ('Real Sociedad'),
+       ('Osasuna'),
+       ('Villarreal'),
+       ('Rayo Vallecano'),
+       ('Celta Vigo'),
+       ('Mallorca'),
+       ('Girona'),
+       ('Almería'),
+       ('Real Valladolid'),
+       ('Getafe'),
+       ('Sevilla'),
+       ('Espanyol'),
+       ('Cádiz'),
+       ('Elche');
+
+insert into seasons (fk_league_id, fk_team_id, year) values (1, 1, 2022);
+
+insert into players (fk_team_id, first_name, last_name, birthday, primary_pos, second_pos)
+values
+    (1, 'Jaume', 'Doménech', '1990-11-05', 'GK', NULL),
+    (1, 'Giorgi', 'Mamardashvili', '2000-09-29', 'GK', NULL),
+    (1, 'Cristian', 'Rivero', '1998-03-28', 'GK', NULL),
+    (1, 'Mouctar', 'Diakhaby', '1996-12-16', 'CB', NULL),
+    (1, 'Gabriel', 'Paulista', '1990-11-26', 'CB', NULL),
+    (1, 'Cenk', 'Özkacar', '2000-10-06', 'CB', NULL),
+    (1, 'Eray', 'Cömert', '1998-02-04', 'CB', NULL),
+    (1, 'Cristhian', 'Mosquera', '2004-06-27', 'CB', NULL),
+    (1, 'José', 'Gayà', '1995-05-25','LB', 'LWB'),
+    (1, 'Jesús', 'Vázquez', '2003-01-02', 'LB', NULL),
+    (1, 'Toni', 'Lato', '1997-11-21', 'LB', 'LM'),
+    (1, 'Thierry', 'Correia', '1999-03-09', 'RB', 'RWB'),
+    (1, 'Dimitri', 'Foulquier', '1993-03-23', 'RB', NULL),
+    (1, 'Hugo', 'Guillamón', '2000-01-31', 'CDM', 'CM'),
+    (1, 'Yunus', 'Musah', '2002-11-29', 'CM', 'CDM'),
+    (1, 'Nico', 'González', '2002-01-03', 'CM', 'CDM'),
+    (1, 'Ilaix', 'Moriba', '2003-01-19', 'CM', NULL),
+    (1, 'André', 'Almeida', '2000-05-30', 'CM', 'CAM'),
+    (1, 'Justin', 'Kluivert', '1999-05-05', 'LW', 'RW'),
+    (1, 'Samuel', 'Lino', '1999-12-23', 'LW', 'ST'),
+    (1, 'Samu', 'Castillejo', '1995-01-18', 'RW', 'LW'),
+    (1, 'Hugo', 'Duro', '1999-11-10', 'ST', 'RW'),
+    (1, 'Marcos', 'André', '1996-10-20', 'ST', 'LW'),
+    (1, 'Edinson', 'Cavani', '1987-02-14', 'ST', NULL),
+    (1, 'Koba', 'Koindredi', '2001-10-27', 'CM', NULL),
+    (1, 'Uroš', 'Račić', '1998-03-17', 'CM', 'CDM'),
+    (1, 'Tiago', 'Ribeiro', '2002-03-14', 'CDM', NULL),
+    (1, 'Pablo', 'Gozálbez', '2001-04-30', 'CAM', 'CM'),
+    (1, 'Vicente', 'Esquerdo', '1999-01-02', 'CM', NULL),
+    (1, 'Jorge', 'Sáenz', '1996-11-17', 'CB', NULL);
+
+
+insert into games (fk_season_id, game_num, home_or_away, fk_opp_id, opp_goals, opp_xg, opp_poss_pct, opp_shots, opp_shots_on_targ, my_goals, my_xg, my_shots, my_shots_on_targ)
+values (1, 1, 'home', '13', 1, 3.8, 54, 16, 13, 3, 1.8, 9, 4);
+
+

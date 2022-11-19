e158700219f668e42ab1bbd7c2fbe73c15189a5f
bhoeft <brandonmhoeft@gmail.com>
2022-11-19 14:09:44 -0600

    update: Spurs career game 1 log PL away vs Brighton

--- database/insert_scripts.sql
+++ database/insert_scripts.sql
@@ -1,114 +1,147 @@
 --  query templates for loading tables
 insert into leagues (name)
 values
-    ('La Liga'),
-    ('Copa Del Rey');
+    ('Premier League'),
+    ('EFL Cup'),
+    ('FA Cup'),
+    ('Champions League'),
+    ('Europa League'),
+    ('Europa Conference League');
 
 insert into teams (name)
-values ('Valencia CF'),
-       ('Real Madrid'),
-       ('Barcelona'),
-       ('Athletic Club'),
-       ('Atletico Madrid'),
-       ('Real Betis'),
-       ('Real Sociedad'),
-       ('Osasuna'),
-       ('Villarreal'),
-       ('Rayo Vallecano'),
-       ('Celta Vigo'),
-       ('Mallorca'),
-       ('Girona'),
-       ('Almería'),
-       ('Real Valladolid'),
-       ('Getafe'),
-       ('Sevilla'),
-       ('Espanyol'),
-       ('Cádiz'),
-       ('Elche');
+values ('Tottenham Hotspur'),
+       ('Arsenal'),
+       ('Aston Villa'),
+       ('Bournemouth'),
+       ('Brentford'),
+       ('Brighton & Hove Albion'),
+       ('Chelsea'),
+       ('Crystal Palace'),
+       ('Everton'),
+       ('Fulham'),
+       ('Leeds United'),
+       ('Leicester City'),
+       ('Liverpool'),
+       ('Manchester City'),
+       ('Manchester United'),
+       ('Newcastle United'),
+       ('Nottingham Forest'),
+       ('Southampton'),
+       ('West Ham United'),
+       ('Wolverhampton Wanderers'),
+       ('Marseille'),
+       ('Eintracht Franfurt'),
+       ('Sporting CP');
 
 insert into seasons (fk_league_id, fk_team_id, year) values (1, 1, 2022);
 
 insert into players (fk_team_id, first_name, last_name, birthday, primary_pos, second_pos)
 values
-    (1, 'Jaume', 'Doménech', '1990-11-05', 'GK', NULL),
-    (1, 'Giorgi', 'Mamardashvili', '2000-09-29', 'GK', NULL),
-    (1, 'Cristian', 'Rivero', '1998-03-28', 'GK', NULL),
-    (1, 'Mouctar', 'Diakhaby', '1996-12-16', 'CB', NULL),
-    (1, 'Gabriel', 'Paulista', '1990-11-26', 'CB', NULL),
-    (1, 'Cenk', 'Özkacar', '2000-10-06', 'CB', NULL),
-    (1, 'Eray', 'Cömert', '1998-02-04', 'CB', NULL),
-    (1, 'Cristhian', 'Mosquera', '2004-06-27', 'CB', NULL),
-    (1, 'José', 'Gayà', '1995-05-25','LB', 'LWB'),
-    (1, 'Jesús', 'Vázquez', '2003-01-02', 'LB', NULL),
-    (1, 'Toni', 'Lato', '1997-11-21', 'LB', 'LM'),
-    (1, 'Thierry', 'Correia', '1999-03-09', 'RB', 'RWB'),
-    (1, 'Dimitri', 'Foulquier', '1993-03-23', 'RB', NULL),
-    (1, 'Hugo', 'Guillamón', '2000-01-31', 'CDM', 'CM'),
-    (1, 'Yunus', 'Musah', '2002-11-29', 'CM', 'CDM'),
-    (1, 'Nico', 'González', '2002-01-03', 'CM', 'CDM'),
-    (1, 'Ilaix', 'Moriba', '2003-01-19', 'CM', NULL),
-    (1, 'André', 'Almeida', '2000-05-30', 'CM', 'CAM'),
-    (1, 'Justin', 'Kluivert', '1999-05-05', 'LW', 'RW'),
-    (1, 'Samuel', 'Lino', '1999-12-23', 'LW', 'ST'),
-    (1, 'Samu', 'Castillejo', '1995-01-18', 'RW', 'LW'),
-    (1, 'Hugo', 'Duro', '1999-11-10', 'ST', 'RW'),
-    (1, 'Marcos', 'André', '1996-10-20', 'ST', 'LW'),
-    (1, 'Edinson', 'Cavani', '1987-02-14', 'ST', NULL),
-    (1, 'Koba', 'Koindredi', '2001-10-27', 'CM', NULL),
-    (1, 'Uroš', 'Račić', '1998-03-17', 'CM', 'CDM'),
-    (1, 'Tiago', 'Ribeiro', '2002-03-14', 'CDM', NULL),
-    (1, 'Pablo', 'Gozálbez', '2001-04-30', 'CAM', 'CM'),
-    (1, 'Vicente', 'Esquerdo', '1999-01-02', 'CM', NULL),
-    (1, 'Jorge', 'Sáenz', '1996-11-17', 'CB', NULL);
+    (1, 'Hugo', 'Lloris', '1986-12-26', 'GK', NULL),
+    (1, 'Fraser', 'Forster', '1988-03-17', 'GK', NULL),
+    (1, 'Brandon', 'Austin', '1999-01-08', 'GK', NULL),
+    (1, 'Cristian', 'Romero', '1998-04-27', 'CB', NULL),
+    (1, 'Eric', 'Dier', '1994-01-15', 'CB', NULL),
+    (1, 'Davinson', 'Sánchez', '1996-06-12', 'CB', NULL),
+    (1, 'Clément', 'Lenglet', '1995-06-17', 'CB', NULL),
+    (1, 'Japhet', 'Tanganga', '1999-03-31', 'CB', 'RB'),
+    (1, 'Ben', 'Davies', '1993-04-24','CB', 'LB'),
+    (1, 'Emerson', 'Royal', '1999-01-14', 'RWB', 'RB'),
+    (1, 'Matt', 'Doherty', '1992-01-16', 'RWB', 'LWB'),
+    (1, 'Djed', 'Spence', '2000-08-09', 'RWB', 'RB'),
+    (1, 'Ryan', 'Sessegnon', '2000-05-18', 'LWB', 'LW'),
+    (1, 'Ivan', 'Perisic', '1989-02-02', 'LWB', 'LM'),
+    (1, 'Oliver', 'Skipp', '2000-09-16', 'CDM', 'CM'),
+    (1, 'Pierre-Emile', 'Höjbjerg', '1995-08-5', 'CM', 'CDM'),
+    (1, 'Rodrigo', 'Bentancur', '1997-06-25', 'CM', 'CDM'),
+    (1, 'Yves', 'Bissouma', '1996-08-30', 'CDM', 'CM'),
+    (1, 'Pape Matar', 'Sarr', '2002-09-14', 'CM', 'CDM'),
+    (1, 'Heung-Min', 'Son', '1992-07-08', 'LW', 'ST'),
+    (1, 'Bryan', 'Gil', '2001-02-11', 'RW', 'LW'),
+    (1, 'Dejan', 'Kulusevski', '2000-04-25', 'RW', 'CAM'),
+    (1, 'Lucas', 'Moura', '1992-08-23', 'RW', 'LW'),
+    (1, 'Harry', 'Kane', '1993-07-28', 'ST', 'CAM'),
+    (1, 'Richarlison', 'de Andrade', '1997-05-10', 'ST', 'LW'),
+    (1, 'Alfie', 'Devine', '2004-08-01', 'CAM', 'LW'),
+    (1, 'Harvey', 'White', '2001-09-19', 'CDM', 'CM'),
+    (1, 'Dane', 'Scarlett', '2004-03-24', 'ST', NULL),
+    (1, 'Troy', 'Parrott', '2002-02-04', 'ST', 'RM'),
+    (1, 'Destiny', 'Udogie', '2002-11-28', 'LWB', 'LM'),
+    (1, 'Sergio', 'Reguilón', '1996-12-16', 'LWB', 'LB'),
+    (1, 'Joe', 'Rodon', '1997-10-22', 'CB', NULL),
+    (1, 'Giovani', 'Lo Celso', '1996-04-09', 'CAM', 'LM'),
+    (1, 'Tanguy', 'Ndombèlé', '1996-12-28', 'CM', 'CAM'),
+    (1, 'Alfie', 'Whiteman', '1998-10-02', 'GK', NULL),
+    (1, 'Harry', 'Winks', '1996-02-02', 'CM', 'CDM');
+
+
+insert into standings_snapshot (fk_season_id, fk_team_id, standings_as_of, points)
+values
+    (1, 1, '2022-08-01', 0), -- Tottenham
+    (1, 2, '2022-08-01', 0), -- Arsenal
+    (1, 3, '2022-08-01', 0), -- Aston Villa
+    (1, 4, '2022-08-01', 0), -- Bournemouth
+    (1, 5, '2022-08-01', 0), -- Brentford
+    (1, 6, '2022-08-01', 0), -- Brighton & Hove Albion
+    (1, 7, '2022-08-01', 0), -- Chelsea
+    (1, 8, '2022-08-01', 0), -- Crystal Palace
+    (1, 9, '2022-08-01', 0), -- Everton
+    (1, 10, '2022-08-01', 0), -- Fulham
+    (1, 11, '2022-08-01', 0), -- Leeds United
+    (1, 12, '2022-08-01', 0), -- Leicester City
+    (1, 13, '2022-08-01', 0), -- Liverpool
+    (1, 14, '2022-08-01', 0), -- Manchester City
+    (1, 15, '2022-08-01', 0), -- Manchester United
+    (1, 16, '2022-08-01', 0), -- Newcastle United
+    (1, 17, '2022-08-01', 0), -- Nottingham Forest
+    (1, 18, '2022-08-01', 0), -- Southampton
+    (1, 19, '2022-08-01', 0), -- West Ham United
+    (1, 20, '2022-08-01', 0); -- Wolverhampton Wanderers
+
+
 
 --------------------------------------------------------------------------------
--- game7 log: home vs. cadiz
+-- game1 log: Away vs. Brighton & Hove Albion
 insert into games (fk_season_id, game_num, game_minutes, home_or_away, fk_opp_id, opp_goals, opp_xg, opp_poss_pct, opp_shots, opp_shots_on_targ, my_goals, my_xg, my_shots, my_shots_on_targ)
-values (1, 7, 20,'home',19,1,1.6,36,8,3,2,1.6,12,5);
+values (1, 1, 20,'away',6,1,3.2,42,17,7,2,2.7,11,7);
 
--- game7 log: home vs. cadiz
+-- game1 log: Away vs. Brighton & Hove Albion
 insert into player_stats_overall (
     fk_player_id,
     fk_game_id,
     rating,
-    minutes)
+    minutes,
+    possession_won,
+    possession_lost)
 values
-    (2, 7,7.1,92), -- Goalie: Mamardashvili
-    (12,7,6.0,66), -- Correia
-    (4, 7,7.5,92), --Diakhaby
-    (7,7,7.6,92), -- Comert
-    (9, 7,7,92), --gaya
-    (14, 7,7.4,92), -- Guillamon
-    (16, 7,6.8,92), -- Nico Gonzalez
-    (18,7,6.7,92), -- Andre Almeida
-    (20, 7,6.9,92), -- Lino
-    (24,7,6.9,45), -- Cavani
-    (19,7,6.2,66), -- Kluivert
+    (10,1,7.8,79,5,1), -- Emerson Royal
+    (4,1,6.3,73,4,1), -- Cristian Romero
+    (5,1,6.6,93,4,2), -- Eric Dier
+    (9,1,6.6,79,2,1), -- Ben Davies
+    (14,1,7.8,93,5,5), -- Ivan Perisic
+    (16,1,7,93,3,4), -- Hojberg
+    (17,1,7.1,93,2,7), -- Bentancur
+    (20,1,6.5,93,1,8), -- Son
+    (24,1,7.4,93,0,8), -- Kane
+    (22,1,6.8,73,0,3), -- Deki
 
-    --(5, 7), -- Paulista
-    --(6,7), --ozkacar
-    --(11, 7), -- Lato
-    --(19,7,), -- Kluivert
-    --(10,7), -- vazquez
-    --(17,7), -- Iliax Moriba
-    --(15,7), -- Musah
-    (13,7,6.1,26), -- Dmitri Foulquier
-    (21,7,6.6,26), -- Castillejo
-    --(22, 7) -- Duro
-    (23, 7,6.2,47) -- Marcos Andre
+    (6,1,5.8,20,2,1), -- Davinson Sanchez
+    (7,1,6.4,14,1,0), -- Clement Lenglet
+    (11,1,6.8,14,0,1), -- Doherty
+--     (12,), -- Djed Spence
+--     (13,), -- Ryan Sessegnon
+--     (15,), -- Skipp
+--     (18,), -- Bissouma
+--     (21,), -- Gil
+--     (23,), -- Lucas Moura
+    (25,1,6.4,20,1,1); -- Richarlison
 
-
-
--- game7 log: home vs. cadiz
+-- game1 log: Away vs. Brighton & Hove Albion
 insert into player_stats_off (
     fk_player_id,
     fk_game_id,
     possession_pct,
-    dribble_att,
-    dribble_compl,
-    dribble_dist,
     dribble_beat,
-    dribble_knock_on,
     goals,
     non_pen_xg,
     shots,
@@ -120,57 +153,69 @@ insert into player_stats_off (
     players_beat_by_passes,
     passes_att,
     passes_compl,
+    passes_compl_press,
     lobs_compl,
     through_balls_compl,
     crosses_att,
     crosses_compl)
 values
-    (12,7,4,10,10,0.3,0,3,0,0,0,0,0,0,0,0,3,11,6,0,3,1,1), -- Correia
-    (4, 7,3,9,9,0.2,1,2,1,0.3,1,1,0,0,0,0,2,9,7,1,0,0,0), --Diakhaby
-    (7,7,4,13,13,0.3,1,3,0,0,0,0,0,0,0,0,4,14,12,0,2,0,0), -- Comert
-    (9, 7,7,21,21,0.5,2,6,0,0.2,1,0,0,0,0,1,5,24,17,0,1,4,0), --gaya
-    (14, 7,12,28,26,0.9,4,11,0,0,0,0,0,0,0,0,19,29,24,0,1,1,0), -- Guillamon
-    (16, 7,6,23,22,0.4,0,5,0,0,0,0,0,0.2,1,4,14,25,23,0,6,0,0), -- Nico Gonzalez
-    (18,7,8,22,20,0.6,1,7,0,0,0,0,0,0,0,0,6,23,20,2,3,0,0), -- Andre Almeida
-    (20, 7,4,14,12,0.3,0,8,0,0.3,3,1,1,0.2,0,2,4,11,10,0,5,0,0), -- Lino
-    (24,7,3,10,9,0.2,0,4,1,0.6,2,1,0,0,0,2,0,8,5,0,2,0,0), -- Cavani
-    (19,7,3,9,8,0.2,0,6,0,0.2,2,1,0,0,0,1,6,9,6,0,3,1,0), -- Kluivert
+    (10,1,5,2,0,0,0,0,0,0.2,0,1,7,18,16,4,0,1,3,1), -- Emerson Royal
+    (4,1,2,0,0,0,0,0,0,0,0,0,1,9,8,1,0,0,0,0), -- Cristian Romero
+    (5,1,3,0,0,0,0,0,0,0,0,0,4,14,12,0,0,0,0,0), -- Eric Dier
+    (9,1,3,0,0,0,0,0,0,0,0,0,3,13,12,1,0,0,1,0), -- Ben Davies
+    (14,1,5,0,0,0,1,1,0,0.1,0,2,14,26,22,2,0,2,1,0), -- Ivan Perisic
+    (16,1,7,0,0,0,0,0,1,0.2,1,2,10,22,19,5,0,2,0,0), -- Hojberg
+    (17,1,9,2,0,0.8,2,1,0,0.5,0,1,12,29,26,4,0,4,0,0), -- Bentancur
+    (20,1,6,1,0,0.2,3,1,0,0,0,1,4,18,16,2,1,3,1,0), -- Son
+    (24,1,5,1,1,1.2,2,2,0,0.3,1,1,8,21,17,4,1,7,0,0), -- Kane
+    (22,1,4,0,1,0.6,3,2,0,0,0,1,6,13,12,5,1,3,0,0), -- Deki
 
-    --(5, 7), -- Paulista
-    --(6,7), --ozkacar
-    --(11, 7), -- Lato
-    --(19,7,), -- Kluivert
-    --(10,7), -- vazquez
-    --(17,7), -- Iliax Moriba
-    --(15,7), -- Musah
-    (13,7,1,3,3,0,0,0,0,0,0,0,0,0.1,0,1,1,3,2,0,1,0,0), -- Dmitri Foulquier
-    (21,7,4,10,9,0.4,1,8,0,0,2,1,0,0,0,0,2,7,5,0,0,0,0), -- Castillejo
-    --(22, 7) -- Duro
-    (23, 7,3,10,9,0.2,0,4,0,0,1,0,0,0,0,0,5,9,8,0,4,0,0) -- Marcos Andre
+    (6,1,0,0,0,0,0,0,0,0,0,0,3,3,2,1,0,0,0,0), -- Davinson Sanchez
+    (7,1,2,0,0,0,0,0,0,0,0,0,1,3,3,0,0,0,0,0), -- Clement Lenglet
+    (11,1,3,0,0,0,0,0,1,0.7,0,2,4,6,4,2,0,0,1,0), -- Doherty
+--     (12,), -- Djed Spence
+--     (13,), -- Ryan Sessegnon
+--     (15,), -- Skipp
+--     (18,), -- Bissouma
+--     (21,), -- Gil
+--     (23,), -- Lucas Moura
+    (25,1,1,1,0,0,0,0,0,0,0,0,3,4,3,0,0,1,0,0); -- Richarlison
 
 
--- game7 log: home vs. cadiz
-insert into player_stats_def (fk_player_id, fk_game_id, stand_tkl_att, stand_tkl_won, slide_tkl_att, slide_tkl_won, interceptions, blocks, clearances, off_duels_att, off_duels_won, def_duels_att, def_duels_won, air_duels_att, air_duels_won, beaten_by_opp, fouls_committed)
+-- game1 log: Away vs. Brighton & Hove Albion
+insert into player_stats_def (
+    fk_player_id,
+    fk_game_id,
+    tackle_att, -- standing + sliding
+    tackle_won, -- standing + sliding
+    interceptions,
+    blocks,
+    clearances,
+    offsides_caused,
+    duels_att, -- off + def duels att
+    duels_won, -- off + def duels won
+    air_duels_att,
+    air_duels_won,
+    beaten_by_opp)
 values
-    (12,7,1,0,1,0,2,0,0,2,0,1,0,0,0,1,0), -- Correia
-    (4, 7,2,1,1,0,2,0,0,2,1,8,6,2,1,3,0), --Diakhaby
-    (7,7,3,3,3,1,2,1,0,2,2,4,3,2,0,0,1), -- Comert
-    (9, 7,1,0,2,0,4,0,0,3,2,8,5,0,0,4,0), --gaya
-    (14, 7,7,0,0,0,1,1,1,8,4,11,4,1,1,1,0), -- Guillamon
-    (16, 7,2,0,0,0,3,0,0,4,2,2,0,1,1,1,0), -- Nico Gonzalez
-    (18,7,3,1,0,0,1,0,0,6,1,4,4,1,0,2,0), -- Andre Almeida
-    (20, 7,2,0,0,0,1,0,0,6,3,3,2,2,1,2,0), -- Lino
-    (24,7,0,0,0,0,0,0,0,05,2,2,2,1,0,0,0), -- Cavani
-    (19,7,0,0,2,0,0,0,0,2,2,4,1,1,0,2,0), -- Kluivert
+    (10,1,2,2,3,0,0,0,6,2,2,2,0), -- Emerson Royal
+    (4,1,2,0,2,2,0,0,6,2,2,1,1), -- Cristian Romero
+    (5,1,2,1,4,0,0,0,6,3,3,2,3), -- Eric Dier
+    (9,1,1,0,1,0,2,0,7,4,4,3,1), -- Ben Davies
+    (14,1,5,3,2,0,0,0,8,3,1,1,2), -- Ivan Perisic
+    (16,1,6,2,1,0,0,0,11,7,1,0,2), -- Hojberg
+    (17,1,7,1,3,1,0,0,15,5,1,0,2), -- Bentancur
+    (20,1,0,0,1,0,0,0,8,3,2,0,2), -- Son
+    (24,1,2,0,0,0,0,0,9,2,0,0,0), -- Kane
+    (22,1,0,0,0,0,0,0,6,4,1,1,0), -- Deki
 
-    --(5, 7), -- Paulista
-    --(6,7), --ozkacar
-    --(11, 7), -- Lato
-    --(19,7,), -- Kluivert
-    --(10,7), -- vazquez
-    --(17,7), -- Iliax Moriba
-    --(15,7), -- Musah
-    (13,7,1,0,1,0,0,0,0,1,1,2,0,1,0,1,0), -- Dmitri Foulquier
-    (21,7,1,1,1,0,1,0,0,3,2,1,1,0,0,0,0), -- Castillejo
-    --(22, 7) -- Duro
-    (23, 7,0,0,0,0,0,0,1,6,4,0,0,1,1,0,0) -- Marcos Andre
+    (6,1,1,0,3,0,0,0,2,1,1,1,0), -- Davinson Sanchez
+    (7,1,1,1,0,0,0,1,1,0,0,0,0), -- Clement Lenglet
+    (11,1,1,0,0,0,0,0,3,1,1,1,1), -- Doherty
+--     (12,), -- Djed Spence
+--     (13,), -- Ryan Sessegnon
+--     (15,), -- Skipp
+--     (18,), -- Bissouma
+--     (21,), -- Gil
+--     (23,), -- Lucas Moura
+    (25,1,1,0,0,0,1,0,1,0,1,1,0); -- Richarlison

82bd15ae334ae524dc79efe4b9512a70d29d1af6
bhoeft <brandonmhoeft@gmail.com>
2022-12-01 12:29:24 -0600

    update: 1st game log away vs braga

--- database/insert_scripts.sql
+++ database/insert_scripts.sql
@@ -91,3 +91,77 @@ values
 
 
 --------------------------------------------------------------------------------
+-- game log 1: away vs SC Braga
+insert into games (
+    fk_season_id,
+    game_num,
+    game_minutes,
+    home_or_away,
+    fk_opp_id,
+    opp_goals,
+    opp_xg,
+    opp_poss_pct,
+    opp_shots,
+    my_goals,
+    my_xg,
+    my_shots
+)
+values
+    (1,1,18,'away',5,1,3,48,15,0,2.4,13);
+
+insert into player_stats (
+    fk_player_id,
+    fk_game_id,
+    rating,
+    minutes,
+    poss_won,
+    poss_lost,
+    dribble_beat,
+    goals,
+    non_pen_xg,
+    shots,
+    assists,
+    xa,
+    second_assists,
+    key_passes,
+    players_beat_passes,
+    passes_att,
+    passes_compl,
+    passes_compl_press,
+    through_balls_compl,
+    crosses_att,
+    crosses_compl,
+    tackles_att,
+    tackles_won,
+    intrcpts_blocks_clears,
+    offsides_caused,
+    duels_att,
+    duels_won,
+    air_duels_att,
+    air_duels_won
+)
+values
+    (10, 1,7.4,92,4,4,1,0,0,0,0,0,0,0,5,12,8,1,2,3,0,3,3,2,0,11,6,2,1), -- João Mário
+    (6, 1, 5.7,23,0,1,1,0,0.01,1,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,0), -- Pepe CB
+    (7, 1, 7.1,92,6,2,3,0,0,0,0,0,0,0,6,15,14,3,1,0,0,8,2,5,0,14,6,5,2), --Iván Marcano
+    (8, 1, 7.1,92,4,3,0,0,0.2,1,0,0,0,1,9,14,11,0,3,1,0,3,0,4,0,7,3,4,1), -- Zaidu Sanusi
+    (13, 1,6.2,71,8,2,1,0,0.01,1,0,0,0,2,3,22,21,8,2,0,0,4,1,4,0,9,2,7,6), -- Mateus Uribe
+    (12, 1, 7, 92,3,6,7,0,0.3,5,0,0,0,0,10,16,13,3,1,2,1,4,3,2,0,16,11,3,0), -- Stephen Eustaquio
+    (17, 1,6.5,92,3,6,2,0,0.5,2,0,0,0,2,2,20,15,5,3,0,0,1,0,4,0,13,8,2,1), -- Otávio Edmilson da Silva Monteiro
+    (18, 1, 5.8,62,0,4,1,0,0.2,1,0,0,0,2,3,14,9,2,4,1,0,1,0,0,0,4,4,0,0), -- Pepê Gabriel Aquino Cossa
+    (23, 1, 6.6,92,0,2,0,0,0,0,0,0.9,0,2,4,6,5,0,2,1,1,0,0,0,0,2,1,1,0), -- Francisco Evanilson
+    (24, 1, 6.5,92,0,5,0,0,1.1,2,0,0,0,0,2,13,8,2,3,1,0,1,0,0,0,4,4,3,1), -- Mehdi Taremi
+
+    (5, 1, 6.6, 69, 2,3,1,0,0,0,0,0,0,1,5,12,10,1,1,0,0,1,0,2,0,5,3,5,2), -- Fábio Cardoso
+    --(4, 1, ...), -- David Carmo
+    --(9, 1, ), -- Wendell Nascimento Borges
+    --(11, 1, ), -- Wilson Manafá
+    --(14, 1, ), -- Marko Grujic
+    --(16, 1, ), -- Bruno Costa
+    (19, 1, 6.3,21,2,1,1,0,0,0,0,0.1,0,1,5,6,5,2,4,0,0,2,1,1,0,2,1,2,2), -- André Franco
+    (20, 1, 5.6,30,0,3,0,0,0,0,0,0,0,0,0,3,1,0,1,0,0,0,0,0,0,1,0,1,0); -- Galeno
+    --(21, 1, ), -- Gabriel Veron
+    --(25, 1, ), -- Toni Martinez
+    --(26, 1, ), -- Danny Namaso Loader
+
+

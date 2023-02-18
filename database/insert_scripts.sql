9ed359b62ffcf139747f5bd563e9045429fd3d7c
bhoeft <brandonmhoeft@gmail.com>
2023-02-18 14:02:47 -0600

    refactor: add werder bremen insert scripts

--- database/insert_scripts.sql
+++ database/insert_scripts.sql
@@ -1,79 +1,75 @@
 --  query templates for loading tables
 insert into leagues (name)
 values
-    ('Premier League'),
-    ('EFL Cup'),
-    ('FA Cup'),
+    ('Bundesliga 2'),
+    ('DFB-Pokal'),
     ('Champions League'),
     ('Europa League'),
     ('Europa Conference League');
 
 insert into teams (name)
-values ('Tottenham Hotspur'),
-       ('Arsenal'),
-       ('Aston Villa'),
-       ('Bournemouth'),
-       ('Brentford'),
-       ('Brighton & Hove Albion'),
-       ('Chelsea'),
-       ('Crystal Palace'),
-       ('Everton'),
-       ('Fulham'),
-       ('Leeds United'),
-       ('Leicester City'),
-       ('Liverpool'),
-       ('Manchester City'),
-       ('Manchester United'),
-       ('Newcastle United'),
-       ('Nottingham Forest'),
-       ('Southampton'),
-       ('West Ham United'),
-       ('Wolverhampton Wanderers'),
-       ('Marseille'),
-       ('Eintracht Franfurt'),
-       ('Sporting CP');
+values ('Werder Bremen'),
+        ('1. FC Köln'),
+        ('1899 Hoffenheim'),
+        ('Bayer Leverkusen'),
+        ('Bayern Munich'),
+        ('Borussia Dortmund'),
+        ('Borussia Mönchengladbach'),
+        ('Eintracht Frankfurt'),
+        ('FC Augsburg'),
+        ('Hamburger SV'),
+        ('Hannover 96'),
+        ('Hertha BSC'),
+        ('Mainz 05'),
+        ('RB Leipzig'),
+        ('SC Freiburg'),
+        ('Schalke 04'),
+        ('VfB Stuttgart'),
+        ('Wolfsburg'),
+        ('Arminia Bielefeld'),
+        ('Eintracht Braunschweig'),
+        ('Darmstadt 98'),
+        ('Fortuna Düsseldorf'),
+        ('Greuther Fürth'),
+        ('Hamburger SV'),
+        ('Hannover 96'),
+        ('1. FC Heidenheim'),
+        ('1. FC Kaiserslautern'),
+        ('Karlsruher SC'),
+        ('Holstein Kiel'),
+        ('1. FC Magdeburg'),
+        ('1. FC Nürnberg'),
+        ('SC Paderborn'),
+        ('Jahn Regensburg'),
+        ('Hansa Rostock'),
+        ('SV Sandhausen'),
+        ('FC St. Pauli')
 
-insert into seasons (fk_league_id, fk_team_id, year) values (1, 1, 2022);
-
-insert into players (fk_team_id, first_name, last_name, birthday, primary_pos, second_pos)
+insert into players (fk_team_id, first_name, last_name, birth_year, primary_pos)
 values
-    (1, 'Hugo', 'Lloris', '1986-12-26', 'GK', NULL),
-    (1, 'Fraser', 'Forster', '1988-03-17', 'GK', NULL),
-    (1, 'Brandon', 'Austin', '1999-01-08', 'GK', NULL),
-    (1, 'Cristian', 'Romero', '1998-04-27', 'CB', NULL),
-    (1, 'Eric', 'Dier', '1994-01-15', 'CB', NULL),
-    (1, 'Davinson', 'Sánchez', '1996-06-12', 'CB', NULL),
-    (1, 'Clément', 'Lenglet', '1995-06-17', 'CB', NULL),
-    (1, 'Japhet', 'Tanganga', '1999-03-31', 'CB', 'RB'),
-    (1, 'Ben', 'Davies', '1993-04-24','CB', 'LB'),
-    (1, 'Emerson', 'Royal', '1999-01-14', 'RWB', 'RB'),
-    (1, 'Matt', 'Doherty', '1992-01-16', 'RWB', 'LWB'),
-    (1, 'Djed', 'Spence', '2000-08-09', 'RWB', 'RB'),
-    (1, 'Ryan', 'Sessegnon', '2000-05-18', 'LWB', 'LW'),
-    (1, 'Ivan', 'Perisic', '1989-02-02', 'LWB', 'LM'),
-    (1, 'Oliver', 'Skipp', '2000-09-16', 'CDM', 'CM'),
-    (1, 'Pierre-Emile', 'Höjbjerg', '1995-08-5', 'CM', 'CDM'),
-    (1, 'Rodrigo', 'Bentancur', '1997-06-25', 'CM', 'CDM'),
-    (1, 'Yves', 'Bissouma', '1996-08-30', 'CDM', 'CM'),
-    (1, 'Pape Matar', 'Sarr', '2002-09-14', 'CM', 'CDM'),
-    (1, 'Heung-Min', 'Son', '1992-07-08', 'LW', 'ST'),
-    (1, 'Bryan', 'Gil', '2001-02-11', 'RW', 'LW'),
-    (1, 'Dejan', 'Kulusevski', '2000-04-25', 'RW', 'CAM'),
-    (1, 'Lucas', 'Moura', '1992-08-23', 'RW', 'LW'),
-    (1, 'Harry', 'Kane', '1993-07-28', 'ST', 'CAM'),
-    (1, 'Richarlison', 'de Andrade', '1997-05-10', 'ST', 'LW'),
-    (1, 'Alfie', 'Devine', '2004-08-01', 'CAM', 'LW'),
-    (1, 'Harvey', 'White', '2001-09-19', 'CDM', 'CM'),
-    (1, 'Dane', 'Scarlett', '2004-03-24', 'ST', NULL),
-    (1, 'Troy', 'Parrott', '2002-02-04', 'ST', 'RM'),
-    (1, 'Destiny', 'Udogie', '2002-11-28', 'LWB', 'LM'),
-    (1, 'Sergio', 'Reguilón', '1996-12-16', 'LWB', 'LB'),
-    (1, 'Joe', 'Rodon', '1997-10-22', 'CB', NULL),
-    (1, 'Giovani', 'Lo Celso', '1996-04-09', 'CAM', 'LM'),
-    (1, 'Tanguy', 'Ndombèlé', '1996-12-28', 'CM', 'CAM'),
-    (1, 'Alfie', 'Whiteman', '1998-10-02', 'GK', NULL),
-    (1, 'Harry', 'Winks', '1996-02-02', 'CM', 'CDM');
-
+    (1, 'Marco', 'Friedl', 1998, 'CB'),
+    (1, 'Amos', 'Pieper', 1998, 'CB'),
+    (1, 'Milos', 'Veljkovic', 1995, 'CB'),
+    (1, 'Niklas', 'Stark', 1995, 'CB'),
+    (1, 'Fabio', 'Chiarodia', 2005, 'CB'),
+    (1, 'Lee', 'Buchanan', 2001, 'LB'),
+    (1, 'Anthony', 'Jung', 1991, 'LB'),
+    (1, 'Mitchel', 'Weiser', 1994, 'RB'),
+    (1, 'Felix', 'Agu', 1999, 'RB'),
+    (1, 'Christian', 'Groß', 1989, 'CM'),
+    (1, 'Ilia', 'Gruev', 2000, 'CM'),
+    (1, 'Jens', 'Stage', 1996, 'CM'),
+    (1, 'Jean Manuel', 'Mbom', 2000, 'CM'),
+    (1, 'Dikeni', 'Salifou', 2003, 'CM'),
+    (1, 'Tom', 'Berger', 2001, 'CM'),
+    (1, 'Romano', 'Schmid', 2000, 'CM'),
+    (1, 'Leonardo', 'Bittencourt', 1993, 'CM'),
+    (1, 'Niklas', 'Schmidt', 1998, 'CM'),
+    (1, 'Niclas', 'Füllkrug', 1993, 'ST'),
+    (1, 'Marvin', 'Ducksch', 1994, 'ST'),
+    (1, 'Oliver', 'Burke', 1997, 'ST'),
+    (1, 'Eren', 'Dinkci', 2001, 'ST'),
+    (1, 'Maximilian', 'Philipp', 1994, 'ST')
 
 insert into standings_snapshot (fk_season_id, fk_team_id, standings_as_of, points, goal_diff)
 values
@@ -100,7 +96,7 @@ values
 
 
 --------------------------------------------------------------------------------
--- game log 6: away vs Man City
+-- game log 1:  vs Hannover
 insert into games (
     fk_season_id,
     game_num,
@@ -116,7 +112,7 @@ insert into games (
     my_shots
 )
 values
-    (1,6,18,'away',14,1,2.9,56,16,2,1.4,9);
+    ();
 
 insert into player_stats (
     fk_player_id,
@@ -125,50 +121,16 @@ insert into player_stats (
     minutes,
     poss_won,
     poss_lost,
-    dribble_beat,
     goals,
     non_pen_xg,
     shots,
     assists,
     xa,
     second_assists,
-    key_passes,
     players_beat_passes,
     passes_att,
     passes_compl,
-    passes_compl_press,
-    through_balls_compl,
-    crosses_att,
-    crosses_compl,
-    tackles_att,
-    tackles_won,
-    intrcpts_blocks_clears,
+    interceptions,
     duels_att,
     duels_won,
-    air_duels_att,
-    air_duels_won
 )
-values
-    (10,6,7.1,93,6,8,2,0,0,0,0,0,0,0,4,22,15,4,1,1,0,1,1,3,15,5,1,0), -- Emerson Royal
-    (4,6,6.8,93,3,5,4,0,0,0,0,0,0,0,7,27,24,4,1,0,0,4,2,3,10,3,0,0), -- Cristian Romero
-    (5,6,6.4,81,6,2,0,0,0,0,0,0,0,0,6,15,13,2,1,0,0,5,1,4,12,5,0,0), -- Eric Dier
-    (7,6,6.9,93,3,2,1,0,0,0,0,0,0,1,11,16,14,3,3,0,0,1,1,3,7,2,1,0), -- Clement Lenglet
-    (13,6,6.9,93,1,1,1,0,0.2,1,0,0,0,0,3,4,4,0,1,0,0,3,1,0,5,2,0,0), -- Ryan Sessegnon
-    (16,6,7.2,81,4,2,2,1,0.2,1,0,0,0,0,4,16,15,4,2,0,0,5,1,2,17,10,0,0), -- Hojberg
-    (17,6,6.6,93,2,5,3,0,0.15,3,0,0,1,0,7,18,15,8,3,0,0,3,0,1,17,10,2,1), -- Bentancur
-    (25,6,5.9,67,0,3,1,0,0,0,0,0,0,0,2,9,6,3,2,0,0,1,0,0,4,0,0,0), -- Richarlison
-    (24,6,6.3,93,0,6,0,0,0.5,3,1,0.1,0,2,4,10,9,2,2,0,0,5,0,0,14,7,3,1), -- Kane
-    (20,6,7.6,93,2,2,0,1,0.3,1,1,0.2,0,3,7,9,6,0,0,2,1,1,0,1,6,1,2,0), -- Son
-
-    (6,6,5.9,12,1,1,1,0,0,0,0,0,0,0,2,3,3,0,0,0,0,1,1,0,3,2,0,0), -- Davinson Sanchez
-    --(9,5,7.2,92,6,2,3,0,0.05,2,0,0.1,0,1,7,12,11,2,1,0,0,6,4,3,12,8,1,0), -- Ben Davies
-    (14,6,6,26,0,1,2,0,0,0,0,0.05,0,1,2,4,3,3,1,0,0,1,0,0,4,3,0,0), -- Ivan Perisic
-
-    --(11,5,6.5,47,0,2,2,0,0,0,0,0,0,0,5,12,10,5,3,1,0,0,0,0,7,4,0,0), -- Doherty
-
-    --(12,), -- Djed Spence
-    --(18,5,7.1,92,7,4,2,0,0,1,0,0.3,0,2,10,24,21,8,2,1,1,3,3,2,8,5,3,3), -- Bissouma
-    (15,6,5.8,12,0,0,0,0,0,0,0,0,0,0,0,4,4,0,1,0,0,0,0,0,0,0,0,0); -- Skipp
-    --(23,6,); -- Lucas Moura
-    --(21,5,6.0,24,0,2,2,0,0.4,2,0,0,0,0,0,3,3,0,1,0,0,0,0,0,5,4,1,0), -- Gil
-    --(22,3,5.9,22,0,3,0,0,0.5,1,0,0,0,0,2,3,1,0,0,0,0,1,0,1,1,0,0,0); -- Deki

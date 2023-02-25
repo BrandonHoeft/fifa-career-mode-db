e490ecd243e340345feaf8c01a58f633c1cc0cf7
bhoeft <brandonmhoeft@gmail.com>
2023-02-24 22:22:50 -0600

    updated: metrics

--- database/metrics.sql
+++ database/metrics.sql
@@ -1,4 +1,3 @@
-
 with raw_stats as (
     select full_name,
            primary_pos,
@@ -7,7 +6,6 @@ with raw_stats as (
            minutes,
            poss_won,
            poss_lost,
-           dribble_beat,
            goals,
            non_pen_xg,
            shots,
@@ -21,8 +19,7 @@ with raw_stats as (
            duels_att,
            duels_won
     from player_stats
-             inner join players p
-                        on player_stats.fk_player_id = p.player_id
+             inner join players p on player_stats.fk_player_id = p.player_id
     order by full_name, fk_game_id
 )
 
@@ -36,7 +33,6 @@ with raw_stats as (
         round(sum(minutes::numeric) / 90, 2) as _90s, -- # of 90 minutes completed
         sum(poss_won) as poss_won_tot,
         sum(poss_lost) as poss_lost_tot,
-        sum(dribble_beat) as dribble_beat_tot,
         sum(shots) as shots_tot,
         sum(passes_att) as pass_att_tot, -- new
         sum(passes_compl) as pass_compl_tot, -- new
@@ -49,7 +45,7 @@ with raw_stats as (
     -- defensive cumulative stats
         sum(interceptions) as interceptions_tot,
         sum(duels_att) as duels_att_tot,
-        sum(duels_won) as duels_won_tot,
+        sum(duels_won) as duels_won_tot
     from raw_stats
     group by full_name, primary_pos
 
@@ -69,7 +65,6 @@ select
             then null
         else round(poss_won_tot::numeric / poss_lost_tot, 2)
     end as poss_won_ratio,
-    round(dribble_beat_tot / _90s, 2) as dribble_beat_per90,
     --shooting
     shots_tot,
     round(shots_tot / _90s, 2) as shots_per90,
@@ -89,13 +84,7 @@ select
     second_assist_tot,
     pbp_tot,
     round (pbp_tot / _90s, 2) as pbp_per90, -- players beat by passes
-    thrgh_ball_tot,
-    round(thrgh_ball_tot / _90s, 2) as thrgh_balls_per90,
     -- defensive
-    tkl_att_tot,
-    tkl_won_tot,
-    round(tkl_won_tot::numeric / nullif(tkl_att_tot, 0), 2) as tkl_win_pct,
-    round(tkl_won_tot / _90s, 2) as tkl_won_per90,
     round(interceptions_tot / _90s, 2) as intercepts_per90,
     duels_won_tot,
     round(duels_won_tot::numeric / nullif(duels_att_tot, 0), 2) as duel_win_pct

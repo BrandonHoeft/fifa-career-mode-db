19718e0970cb5f8bd4a5ff43910e843f4520d749
Brandon Hoeft <13879928+BrandonHoeft@users.noreply.github.com>
2023-08-11 11:23:19 -0500

    Update metrics.sql
    new: metrics updated to reflect new database model

--- database/metrics.sql
+++ database/metrics.sql
@@ -1,42 +1,83 @@
--- starting pass at offensive rate metrics
-with offense as (
-    select off_id,
-           off.fk_game_id,
-           p.full_name,
-           p.primary_pos,
-           ovr.minutes,
-           ovr.rating,
-           possession_pct,
-           dribble_att,
-           dribble_compl,
-           dribble_dist,
-           dribble_beat,
-           dribble_knock_on,
+-- game metrics
+select
+    seasons.year,
+    l.name as league_name,
+    g.game_id,
+    -- generic game info
+    g.game_minutes,
+    g.home_or_away as venue,
+    g.my_goals,
+    g.opp_goals,
+    case
+        when g.my_goals > g.opp_goals then 'Win'
+        when g.opp_goals > g.my_goals then 'Loss'
+        else 'Draw'
+        end as result,
+    case
+        when g.my_goals > g.opp_goals then 3
+        when g.opp_goals > g.my_goals then 0
+        else 1
+        end as points,
+    -- my team's info
+    my_team.name as my_team,
+    g.my_xg,
+    g.my_shots,
+    g.my_poss_pct,
+    -- opponent info
+    opp_team.name as opponent,
+    g.opp_xg,
+    g.opp_shots
+from games g
+inner join seasons
+    on g.fk_season_id = seasons.season_id
+inner join leagues l on
+    seasons.fk_league_id = l.league_id
+inner join teams my_team
+    on seasons.fk_team_id = my_team.team_id
+inner join teams opp_team
+    on g.fk_opp_id = opp_team.team_id
+
+
+
+
+
+
+
+
+-- player metrics
+with raw_stats as (
+    select full_name,
+           primary_pos,
+           fk_game_id,
+           rating,
+           minutes,
+           poss_won,
+           poss_lost,
            goals,
-           non_pen_xg,
+           /* Giving FIFA's xG a haircut
+            real world non-penalty xG per shot = 0.10.
+            sample FIFA non-penalty xG per shot = 0.22. Derived from player_stats table: round(non_pen_xg / nullif(shots, 0), 2)
+            ratio of 0.10 / 0.22 = 45% haircut to get more realistic FIFA xG outputs
+            */
+           round(non_pen_xg * 0.45, 2) as non_pen_xg, -- downweight the FIFA engine's xG
            shots,
-           shots_on_targ,
            assists,
-           xa,
-           second_assists,
            key_passes,
-           players_beat_by_passes,
            passes_att,
            passes_compl,
-           lobs_compl,
-           through_balls_compl,
-           crosses_att,
-           crosses_compl
-    from player_stats_off off
-inner join players p
-    on off.fk_player_id = p.player_id
-        inner join player_stats_overall ovr on
-        off.fk_player_id = ovr.fk_player_id
-        and off.fk_game_id = ovr.fk_game_id
-order by full_name, fk_game_id
+           duels_att,
+           duels_won
+    from player_stats
+    inner join players p on player_stats.fk_player_id = p.player_id
+    /* below is if I want to pre-filter to a particular tournament */
+    -- inner join games g on player_stats.fk_game_id = g.game_id
+    -- inner join seasons s on g.fk_season_id = s.season_id
+    -- inner join leagues l on s.fk_league_id = l.league_id
+    -- where l.name = 'Champions League'
+    order by full_name, fk_game_id
 )
 
-, cum_offense as (
+, cum_stats as (
 
     select
         full_name,
@@ -44,22 +85,19 @@ order by full_name, fk_game_id
         sum(minutes) as total_minutes,
         round(sum(minutes * rating) / sum(minutes), 2) as weighted_rating,
         round(sum(minutes::numeric) / 90, 2) as _90s, -- # of 90 minutes completed
-        sum(possession_pct) as poss_pct_tot,
-        sum(dribble_compl) as dribble_compl_tot,
-        case
-            when sum(dribble_att) = 0
-                then 1 -- used as a denominator in other metrics, can't divide by 0
-            else sum(dribble_att)
-            end as dribble_att_tot,
-        sum(dribble_beat) as dribble_beat_tot,
+        sum(poss_won) as poss_won_tot,
+        sum(poss_lost) as poss_lost_tot,
         sum(shots) as shots_tot,
+        sum(non_pen_xg) as npxg_tot,
+        sum(passes_att) as pass_att_tot, -- new
+        sum(passes_compl) as pass_compl_tot, -- new
         sum(goals) as goals_tot,
-        sum(non_pen_xg) as xg_tot,
         sum(assists) as assist_tot,
-        sum(xa) as xa_tot,
-        sum(players_beat_by_passes) as pbp_tot,
-        sum(through_balls_compl) as thball_compl_tot
-    from offense
+        sum(key_passes) as key_pass_tot,
+    -- defensive cumulative stats
+        sum(duels_att) as duels_att_tot,
+        sum(duels_won) as duels_won_tot
+    from raw_stats
     group by full_name, primary_pos
 
 )
@@ -70,103 +108,30 @@ select
     total_minutes,
     _90s,
     weighted_rating,
-    --dribbling
-    round(poss_pct_tot / _90s, 2) as poss_pct_per90,
-    dribble_compl_tot,
-    round(dribble_compl_tot / _90s, 2) as dribble_compl_per90,
-    round(dribble_compl_tot::numeric / dribble_att_tot, 2) as dribble_succ_rt, -- nulls if 0 dribbles completed
-    round(dribble_beat_tot / _90s, 2) as dribble_beat_per_90,
-    round(dribble_beat_tot::numeric / dribble_att_tot, 2) as dribble_beat_rt,
+    -- possession
+    round(poss_won_tot / _90s, 2) as poss_won_per_90,
+    round(poss_lost_tot / _90s, 2) as poss_lost_per_90,
+    round(poss_won_tot / _90s, 2) - round(poss_lost_tot / _90s, 2) as net_poss_per90,
     --shooting
     shots_tot,
     round(shots_tot / _90s, 2) as shots_per90,
     goals_tot,
     round(goals_tot / _90s, 2) as goals_per90,
-    xg_tot,
-    round(xg_tot / _90s, 2) as xg_per90,
-    --passing
+    npxg_tot,
+    round(npxg_tot / _90s, 2) as npxg_per90,
+    round(npxg_tot / nullif(shots_tot,0), 2) as npxg_per_shot, -- indicator of shot quality
+    round(goals_tot - npxg_tot, 2) as goals_minus_npxg, -- luck/skill relative to goals
+    --passing / chance creation
+    round(pass_compl_tot * 4.5 / _90s, 2) as pass_compl_per90, --4.5 is extrapolating 20 minutes of real game time
+    round(pass_compl_tot::numeric / nullif(pass_att_tot,0),2) as pass_compl_pct,
     assist_tot,
-    xa_tot,
-    round(xa_tot / _90s, 2) as xa_per90,
-    pbp_tot,
-    round (pbp_tot / _90s, 2) as pbp_per90, -- players beat by passes
-    thball_compl_tot,
-    round(thball_compl_tot / _90s, 2) as thballs_compl_per90
-from cum_offense
-where primary_pos like any (array['%W', '%M', 'ST'])
-
-
-
-
-
-
--- starting pass at defensive rate metrics
-with defense as (
-    select def_id,
-           def.fk_game_id,
-           p.full_name,
-           p.primary_pos,
-           p.second_pos,
-           ovr.minutes,
-           stand_tkl_att,
-           stand_tkl_won,
-           slide_tkl_att,
-           slide_tkl_won,
-           interceptions,
-           blocks,
-           clearances,
-           off_duels_att,
-           off_duels_won,
-           def_duels_att,
-           def_duels_won,
-           air_duels_att,
-           air_duels_won,
-           beaten_by_opp,
-           fouls_committed
-    from player_stats_def def
-         inner join players p on def.fk_player_id = p.player_id
-         inner join player_stats_overall ovr on def.fk_player_id = ovr.fk_player_id
-                and def.fk_game_id = ovr.fk_game_id
-)
-
-, cum_defense as (
-
-    select
-        full_name,
-        primary_pos,
-        second_pos,
-        sum(minutes) as total_minutes,
-        round(sum(minutes::numeric) / 90, 2) as _90s, -- # of 90 minutes completed
-        sum(stand_tkl_att) as stand_tkl_att_tot,
-        sum(stand_tkl_won) as stand_tkl_won_tot,
-        sum(slide_tkl_att) as slide_tkl_att_tot,
-        sum(slide_tkl_won) as slide_tkl_won_tot,
-        sum(def_duels_att) as def_duels_att_tot,
-        sum(def_duels_won) as def_duels_won_tot,
-        sum(air_duels_att) as air_duels_att_tot,
-        sum(air_duels_won) as air_duels_won_tot
-    from defense
-    group by full_name, primary_pos, second_pos
-
-)
-
-select
-    full_name,
-    primary_pos,
-    second_pos,
-    _90s,
-    round((stand_tkl_won_tot + slide_tkl_won_tot) / _90s, 2) as succ_tkl_tot_per90,
-    stand_tkl_att_tot,
-    stand_tkl_won_tot,
-    round(stand_tkl_won_tot::numeric / coalesce(nullif(stand_tkl_att_tot, 0), 1), 2) as stand_tkl_succ_rt,
-    slide_tkl_att_tot,
-    slide_tkl_won_tot,
-    round(slide_tkl_won_tot::numeric / coalesce(nullif(slide_tkl_att_tot, 0), 1), 2) as slide_tkl_succ_rt,
-    def_duels_att_tot,
-    def_duels_won_tot,
-    round(def_duels_won_tot::numeric / coalesce(nullif(def_duels_att_tot, 0), 1), 2) as def_duels_succ_rt,
-    air_duels_att_tot,
-    air_duels_won_tot,
-    round(air_duels_won_tot::numeric / coalesce(nullif(air_duels_att_tot, 0), 1), 2) as air_duels_succ_rt
-from cum_defense
-where primary_pos like any (array['%B', 'C%M'])
+    round(assist_tot / _90s, 2) as assists_per90,
+    key_pass_tot,
+    round(key_pass_tot / _90s, 2) as key_passes_per90,
+    100 * round((assist_tot + key_pass_tot)::numeric / nullif(pass_att_tot, 0), 2) as SCA_per_100_passes,
+    -- recovery/challenges
+    round(duels_att_tot / _90s, 2) as duels_att_per90,
+    round(duels_won_tot / _90s, 2) as duels_won_per90,
+    round(duels_won_tot::numeric / nullif(duels_att_tot, 0), 2) as duel_win_pct
+from cum_stats
+--where primary_pos like any (array['%B', 'C%M'])

3fe3f93299c326d07d82617debf735151b865873
bhoeft <brandonmhoeft@gmail.com>
2023-12-22 15:46:06 -0600

    update: streanlit app, sidebar radio buttons

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,11 +2,18 @@ import streamlit as st
 from components.game_form import game_form
 from components.player_stats_form import player_stats_form
 
+# Configure Page
+st.set_page_config(layout="wide")
+
 # Streamlit app title
 st.title('FIFA Career Mode CRUD App')
 
 # Toggle between 'Game' and 'Player Stats' forms
-form_type = st.radio("Choose the type of data to insert:", ('Game', 'Player Stats'))
+with st.sidebar:
+    form_type = st.radio(
+            "Choose the type of data to insert:",
+            ('Game', 'Player Stats')
+    )
 
 if form_type == 'Game':
     game_form()

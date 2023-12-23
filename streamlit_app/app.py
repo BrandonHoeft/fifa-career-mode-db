eb9d210ec179e5f7c8d2fac8116605fae2f778ce
bhoeft <brandonmhoeft@gmail.com>
2023-12-23 15:27:55 -0600

    add: precision on possession pct, pass pct to handle 100%

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from components.game_form import game_form
 from components.player_stats_form import player_stats_form
+from components.create_player import create_player_form
 
 # Configure Page
 st.set_page_config(layout="wide")
@@ -12,10 +13,12 @@ st.title('FIFA Career Mode CRUD App')
 with st.sidebar:
     form_type = st.radio(
             "Choose the type of data to insert:",
-            ('Game', 'Player Stats')
+            ('Game', 'Player Stats', 'Manage Players')
     )
 
 if form_type == 'Game':
     game_form()
 elif form_type == 'Player Stats':
     player_stats_form()
+elif form_type == 'Manage Players':
+    create_player_form()

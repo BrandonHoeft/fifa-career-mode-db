09d9737758878918a4eecef57fc5d23774e6f9a3
bhoeft <brandonmhoeft@gmail.com>
2024-01-14 00:11:54 -0600

    update: add dropdown for scaling the size of bubbles dynamically!
    refactored the graph to handle different size problems. woof. thanks chatgpt

--- streamlit_app/components/plots.py
+++ streamlit_app/components/plots.py
@@ -4,46 +4,50 @@ import streamlit as st
 from queries import query_player_metrics
 
 def interactive_scatterplot():
-    st.title("Player Performance Analytics")
-
     df = query_player_metrics()
+    metric_cols = df.copy().drop(columns=['full_name', 'primary_pos', 'pos_type', 'player_img_url']).columns
 
     # Default column names for X and Y axes
     default_x_column = 'total_minutes'
     default_y_column = 'weighted_rating'
 
-    x_axis_val = st.selectbox('Select X-Axis', options=df.columns, index=df.columns.get_loc(default_x_column) if default_x_column in df.columns else 0)
-    y_axis_val = st.selectbox('Select Y-Axis', options=df.columns, index=df.columns.get_loc(default_y_column) if default_y_column in df.columns else 1)
+    x_axis_val = st.selectbox('Select X-Axis', options=metric_cols, index=metric_cols.get_loc(default_x_column) if default_x_column in df.columns else 0)
+    y_axis_val = st.selectbox('Select Y-Axis', options=metric_cols, index=metric_cols.get_loc(default_y_column) if default_y_column in df.columns else 1)
 
-    # Radio button for color selection
-    color_option = st.radio("Color Options", ['None', 'By Position Type'])
+    # Dropdown for size parameter
+    size_options = ['None'] + list(metric_cols)
+    size_val = st.selectbox('Mark Size Scaler (Optional)', options=size_options, index=0)
 
     # Define color mapping for 'player_type'
     color_map = {
-        'Goalie': 'White',
-        'Defender': 'Orange',
-        'Midfielder': 'Blue',
-        'Attacker': 'Green'
+        'Goalie': '#E2E2E2',  # Gray
+        'Defender': '#FFA15A',  # Orange
+        'Midfielder': '#316395',  # Blue
+        'Attacker': '#1CBE4F'  # Green
     }
 
-    # Conditionally create the plot based on color option
-    if color_option == 'By Position Type':
-        plot = px.scatter(df, x=x_axis_val, y=y_axis_val, color='pos_type',
-                          color_discrete_map=color_map,
-                          hover_data=['full_name', 'primary_pos'],
-                          marginal_x='violin',
-                          marginal_y='violin',
-                          opacity=0.5, size_max=100,
-                          title=f'{x_axis_val} versus {y_axis_val}')
-    else:
-        plot = px.scatter(df, x=x_axis_val, y=y_axis_val,
-                          hover_data=['full_name', 'primary_pos'],
-                          marginal_x='violin',
-                          marginal_y='violin',
-                          opacity=0.5, size_max=100,
-                          title=f'{x_axis_val} versus {y_axis_val}')
+    plot_args = {
+        'x': x_axis_val,
+        'y': y_axis_val,
+        'color': 'pos_type',
+        'color_discrete_map': color_map,
+        'hover_name': 'full_name',
+        'hover_data': ['primary_pos'],
+        'marginal_x': 'box',
+        'marginal_y': 'box',
+        'title': f'{x_axis_val} versus {y_axis_val}'
+    }
+
+    # Conditionally add the size parameter
+    if size_val != 'None':
+        plot_args['size'] = size_val
+
+    plot = px.scatter(df, **plot_args)
+
+    # use a default sizing
+    if size_val == 'None':
+        plot.update_traces(marker=dict(size=12))
+
+    plot.update_traces(opacity=0.75, selector=dict(mode='markers'))
 
     st.plotly_chart(plot)
-
-
-
